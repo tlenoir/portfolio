2dee7f336c5e2feef8c69ccc46c881c57542317f
tlenoir <tlenoir.tcc@gmail.com>
2019-03-03 11:26:49 -1000

    create new AppPage contains the DOM principal

---
+++ src/components/App/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AppComponent extends Component {
+  render() {
+    return (
+      <div>
+        <center><h1>Not yet available!</h1></center>
+      </div>
+    )
+  }
+}
