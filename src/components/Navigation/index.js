2b53f2bfec4aad61e8744f579d3ae1bec6044a3b
tlenoir <tlenoir.tcc@gmail.com>
2019-03-05 16:43:09 -1000

    navbar

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -5,20 +5,33 @@ export default class Navigation extends Component {
   render() {
     return (
       <div>
-        <ul className="nav justify-content-center">
-            <li className="nav-item">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="#">Navbar</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto" data-toggle="collapse" data-target="#navbarSupportedContent">
+              <li className="nav-item">
                 <Link className="nav-link active" to='/animation'>Animation</Link>
-            </li>
-            <li className="nav-item">
+              </li>
+              <li className="nav-item">
                 <Link className="nav-link active" to='/gallery'>Gallery</Link>
-            </li>
-            <li className="nav-item">
+              </li>
+              <li className="nav-item">
                 <Link className="nav-link active" to='/shop'>Shop</Link>
-            </li>
-            <li className="nav-item">
+              </li>
+              <li className="nav-item">
                 <Link className="nav-link active" to='/animation'>Movie</Link>
-            </li>
-        </ul>
+              </li>
+            </ul>
+            <form className="form-inline my-2 my-lg-0">
+              <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+          </div>
+        </nav>
       </div>
     )
   }
