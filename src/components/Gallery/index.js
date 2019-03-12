9e1359b9fb0cda0a3e7dd981d72921fd9ae9a1f2
tlenoir <tlenoir.tcc@gmail.com>
2019-03-12 12:03:41 -1000

    whats do you want to watch? tahiti? moorea?

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -11,18 +11,23 @@ class GalleryComponent extends Component {
     }
 }
 function Unsplash() {
+    const total_pages = 0
     const img = new Image()
     const [picture, setPicture] = useState([])
+    const [input, setInput] = useState({
+        value: 'tahiti'
+    })
+    const [search, setSearch] = useState('')
 
     useEffect(() => {
-        fetch("https://api.unsplash.com/search/photos?query=tahiti", {
+        fetch(`https://api.unsplash.com/search/photos?query=${input.value}`, {
             headers: {
                 "Authorization": `Client-ID ${process.env.REACT_APP_UNSPLASH_API_KEYS}`
             }
         }).then((response) => {
             return response.json();
         }).then(response => {
-            return response.results.map(function(r) {
+            return response.results.map(function (r) {
                 img.src = r.urls.small
                 return {
                     src: img.src,
@@ -30,13 +35,49 @@ function Unsplash() {
                     height: img.height
                 }
             })
+
         }).then(data => {
-            console.log('data', data)
             setPicture(data)
         });
-    }, []);
+    }, [search]);
+    const printValue = e => {
+        e.preventDefault()
+        setSearch(input.value)
+        console.log(input.value)
+    };
+    const updateField = e => {
+        setInput({
+            ...input,
+            value: e.target.value
+        })
+    };
     return (
-        <Gallery photos={picture} />
+        <>
+            <center>
+                <form onSubmit={printValue}>
+                    <input
+                        name="value"
+                        value={input.value}
+                        onChange={updateField} />
+                    <button className="btn btn-primary">Click</button>
+                </form>
+                {/* <nav aria-label="Page navigation example">
+                    <ul className="pagination">
+                        {
+                            Array(picture.total_pages).map((num) =>
+                                <li className="page-item">
+                                    <span className="page-link">
+                                        {num}
+                                    </span>
+                                </li>
+                            )
+                        }
+                    </ul>
+                </nav> */}
+            </center>
+
+            <Gallery photos={picture} />
+        </>
     )
 }
 export default GalleryComponent;
